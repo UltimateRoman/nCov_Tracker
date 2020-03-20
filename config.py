1bccbeba8f20bbb3ce1d7f12b5617d0b6de4e84c
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-03-20 19:10:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 url = "https://corona.lmao.ninja/all"
 url1 = "https://corona.lmao.ninja/countries"
 url2 = "https://api.rootnet.in/covid19-in/stats/latest"
+url3 = "https://corona.lmao.ninja/states"
